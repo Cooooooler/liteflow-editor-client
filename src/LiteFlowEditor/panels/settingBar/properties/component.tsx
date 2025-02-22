83584fae69db4e7b2586da8959ccace8df4f6bed
Lumengling <3422056558@qq.com>
2025-02-22 11:53:01 +0800

    feat: 高亮表单配置

--- src/LiteFlowEditor/panels/settingBar/properties/component.tsx
+++ src/LiteFlowEditor/panels/settingBar/properties/component.tsx
@@ -1,7 +1,7 @@
+import { Checkbox, Form, Input } from 'antd';
+import { debounce } from 'lodash';
 import React from 'react';
-import {Form, Input} from 'antd';
-import {debounce} from 'lodash';
-import {history} from '../../../hooks/useHistory';
+import { history } from '../../../hooks/useHistory';
 import ELNode from '../../../model/node';
 import styles from './index.module.less';
 
@@ -9,8 +9,26 @@ interface IProps {
   model: ELNode;
 }
 
+const handleConfig = (config: string[]) => {
+  const obj: Record<string, boolean> = {};
+  config.forEach((item) => {
+    obj[item] = true;
+  });
+  return obj;
+};
+
+const handleConfigValue = (config: Record<string, boolean>) => {
+  const arr: string[] = [];
+  Object.keys(config).forEach((key) => {
+    if (config[key]) {
+      arr.push(key);
+    }
+  });
+  return arr;
+};
+
 const ComponentPropertiesEditor: React.FC<IProps> = (props) => {
-  const {model} = props;
+  const { model } = props;
   const properties = model.getProperties();
 
   const [form] = Form.useForm();
@@ -18,42 +36,61 @@ const ComponentPropertiesEditor: React.FC<IProps> = (props) => {
   const handleOnChange = debounce(async () => {
     try {
       const changedValues = await form.validateFields();
-      const { id, ...rest } = changedValues
+      const { id, config, ...rest } = changedValues;
+      const { highlight = false } = handleConfig(config);
+
       model.id = id;
-      model.setProperties({...properties, ...rest});
-      history.push(undefined, {silent: true});
+      model.highlight = highlight;
+      model.setProperties({ ...properties, ...rest });
+      history.push(undefined, { silent: true });
       // history.push();
       // 以下是对AntV X6视图层进行临时修改
       const modelNode = model.getStartNode();
       const originSize = modelNode.getSize();
+      const body = modelNode.getAttrs()?.body;
       modelNode
-        .updateAttrs({label: { text: id }})
+        .updateAttrs({ label: { text: id }, body: { ...body, highlight } })
         .setSize(originSize); // 解决由于文本修改导致的尺寸错误
     } catch (errorInfo) {
       console.log('Failed:', errorInfo);
     }
   }, 200);
 
+  const cellOptions = [{ label: 'highlight', value: 'highlight' }];
+
   return (
     <div className={styles.liteflowEditorPropertiesEditorContainer}>
       <Form
         layout="vertical"
         form={form}
-        initialValues={{...properties, id: model.id}}
+        initialValues={{
+          ...properties,
+          id: model.id,
+          config: handleConfigValue({ highlight: model.highlight ?? false }),
+        }}
         onValuesChange={handleOnChange}
         // onBlur={handleOnChange}
       >
         <Form.Item name="id" label="ID">
-          <Input allowClear/>
+          <Input allowClear />
         </Form.Item>
         <Form.Item name="data" label="参数（data）">
-          <Input allowClear/>
+          <Input allowClear />
         </Form.Item>
         <Form.Item name="tag" label="标签（tag）">
-          <Input allowClear/>
+          <Input allowClear />
         </Form.Item>
         <Form.Item name="maxWaitSeconds" label="超时(maxWaitSeconds)">
-          <Input allowClear/>
+          <Input allowClear />
+        </Form.Item>
+        <Form.Item name="config" label="节点配置">
+          <Checkbox.Group>
+            {cellOptions.map(({ label, value }) => (
+              <Checkbox key={value} value={value}>
+                {label}
+              </Checkbox>
+            ))}
+          </Checkbox.Group>
         </Form.Item>
       </Form>
     </div>
